4f36f8bf1fb058208844188197f166645d0b101a
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2025-08-11 21:34:36 +0700

    fix: war mysql

--- core/models/game.py
+++ core/models/game.py
@@ -18,7 +18,7 @@ class Game(TimeInfo):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255, unique=True)
     status = models.SmallIntegerField(default=1, choices=PUBLISH_STATUS)
-    slug = models.SlugField(max_length=300, unique=True, null=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
     javascript = models.TextField()
     css = models.TextField()
     html = models.TextField()
